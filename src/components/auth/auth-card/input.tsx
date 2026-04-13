9024caa1c454cb3dcdc4d6c2ba59d6c011a42e77
MatheusBourguignonF <teusaoboladao@gmail.com>
2026-04-13 18:16:22 -0300

    fix: make AuthInput props optional to fix build error

--- src/components/auth/auth-card/input.tsx
+++ src/components/auth/auth-card/input.tsx
@@ -1,13 +1,13 @@
 interface IAuthInput {
     placeholder: string;
     type: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const AuthInput = (props:IAuthInput) => {
     const {placeholder, type, value, onChange} = props;
     return(
-        <input value={value} onChange={onChange} type={type} placeholder={placeholder} className="border-b-2 border-[#dddddd86] border- w-full h-20 placeholder-[#9D9D9D] placeholder:font-bold p-8 placeholder:italic focus:outline-none text-[#9D9D9D] font-bold"/>
+        <input value={value ?? ""} onChange={onChange} type={type} placeholder={placeholder} className="border-b-2 border-[#dddddd86] border- w-full h-20 placeholder-[#9D9D9D] placeholder:font-bold p-8 placeholder:italic focus:outline-none text-[#9D9D9D] font-bold"/>
     )
 }
